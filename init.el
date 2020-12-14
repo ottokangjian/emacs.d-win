ef7039eaea3c63c352bbe0ef2af5f9d596353a5b
康健 <kangjiankangjian@gmail.com>
2020-12-14 18:10:31 +0800

    Update init.el

--- init.el
+++ init.el
@@ -177,10 +177,10 @@
 (global-visual-line-mode 1)
 
 ;; 【TOC-org】
-(if (require 'toc-org nil t)
-    (add-hook 'org-mode-hook 'toc-org-mode)
+;; (if (require 'toc-org nil t)
+;;     (add-hook 'org-mode-hook 'toc-org-mode)
 
 ;; 【enable in markdown, too】
-    (add-hook 'markdown-mode-hook 'toc-org-mode)
-    (define-key markdown-mode-map (kbd "\C-c\C-o") 'toc-org-markdown-follow-thing-at-point)
-  (warn "toc-org not found"))
+;;    (add-hook 'markdown-mode-hook 'toc-org-mode)
+;;    (define-key markdown-mode-map (kbd "\C-c\C-o") 'toc-org-markdown-follow-thing-at-point)
+;;   (warn "toc-org not found"))
