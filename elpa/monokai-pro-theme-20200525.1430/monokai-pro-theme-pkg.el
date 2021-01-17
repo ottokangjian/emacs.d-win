32ce4f87824b9af0cd807a39ab9f3073ae9a0749
康健 <kangjiankangjian@gmail.com>
2021-01-17 15:40:43 +0800

    2021-01-17-15-40-39

---
+++ elpa/monokai-pro-theme-20200525.1430/monokai-pro-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "monokai-pro-theme" "20200525.1430" "A simple theme based on the Monokai Pro Sublime color schemes" 'nil :commit "d1bc669200bf5753cf1963e5e65269e0d60648d5" :authors '(("Kaleb Elwert" . "belak@coded.io")) :maintainer '("Kaleb Elwert" . "belak@coded.io") :url "https://github.com/belak/emacs-monokai-pro-theme")
